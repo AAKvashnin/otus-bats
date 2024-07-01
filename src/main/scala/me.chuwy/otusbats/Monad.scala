1a257e62192ae1cd4922f2fb49d4e8b55acda78f
AAKvashnin <alexey.kvashnin@gmail.com>
2024-07-01 23:26:23 +0300

    implement Monad instances

--- src/main/scala/me.chuwy/otusbats/Monad.scala
+++ src/main/scala/me.chuwy/otusbats/Monad.scala
@@ -10,7 +10,28 @@ trait Monad[F[_]] extends Functor[F] { self =>
 
 object Monad {
 
-   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] = self.map(fa)(f).flatten
+ implicit def optionMonad(implicit fun:Functor[Option]):Monad[Option] = new Monad[Option] {
 
+   override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa match {
+     case None => None
+     case Some(a)=>Some(f(a))
+   }
+
+   override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fun.map(fa)(f).flatten
+
+   override def point[A](a:A):Option[A]=Some(a)
+
+
+ }
+
+ implicit def listMonad(implicit fun:Functor[List]):Monad[List] = new Monad[List] {
+
+   override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+
+   override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fun.map(fa)(f).flatten
+
+   override def point[A](a:A):List[A]=List(a)
+
+ }
 
 }
